6bdad0199f002d5b8f2ad80709af3215777fd675
18HenryFloyd18 <henryfloyd50@gmail.com>
2025-01-29 16:29:09 -0600

    Add files via upload

---
+++ Variables_assignment.py
@@ -0,0 +1,20 @@
+Username = str(input("What is you're name?:"))#Ask For User's Name
+Userage = int(input("What is you're age?:"))#Ask For User's Age
+print(Username)#Print User's Name
+print(Userage)#Print User's Age
+if(Userage == 13):#If User's age is 13 print this if not print this
+    print(["Bannana","Orange","13",])
+else:
+    print(["Apple","Blueberry","27"])
+if(Username == "Henry" or "henry"):#If the user name is henry print this if not print this
+    print(True)
+else:
+    print(False)
+if(Userage >= 7):#If user's age is equal to or greater than 7 print this if not print this
+    print(5.5)
+else:
+    print(11.53)
+
+
+
+ 
